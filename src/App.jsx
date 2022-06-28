b333c232d9708b73e9ec055b8455c6e3cc4e14a3
unknown <lucastheborges@gmail.com>
2022-06-27 21:24:03 -0300

    Exluido arquivos que não serão utilizados no projeto

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import {Post} from "./Post";
+
+export function App() {
+  return (
+    <div>
+      <Post 
+        author='Lucas Alberty'
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit."
+      />
+      <Post 
+        author='Alberty Lucas'
+        content="Lorem ipsum dolor sit amet consectetur adipisicin elit."
+      />
+      
+      
+    </div>
+  );
+}
+
+
