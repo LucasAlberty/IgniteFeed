b6396535a069bee9bdde2446b8e49548d0333641
unknown <lucastheborges@gmail.com>
2022-07-14 21:01:51 -0300

    Posicionamento dos elementos da pagina e estilização da sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,27 @@
 import { Header } from "./components/Header";
-import {Post} from "./Post";
-import './global.css'
+import { Post } from "./Post";
+import {Sidebar} from "./components/Sidebar"
+import "./global.css";
+import styles from "./App.module.css";
 export function App() {
   return (
     <div>
-      <Header/>
+      <Header />
 
-      <Post 
-        author='Lucas Alberty'
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit."
-      />
-      <Post 
-        author='Alberty Lucas'
-        content="Lorem ipsum dolor sit amet consectetur adipisicin elit."
-      />
-      
-      
+      <div className={styles.wrapper}>
+        <Sidebar />
+
+        <main>
+          <Post
+            author="Lucas Alberty Borges"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit."
+          />
+          <Post
+            author="Alberty Lucas"
+            content="Lorem ipsum dolor sit amet consectetur adipisicin elit."
+          />
+        </main>
+      </div>
     </div>
   );
 }
-
-
