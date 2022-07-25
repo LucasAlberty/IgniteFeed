8ae74e030b401430cebfa497590c4fbd39e3cfdc
unknown <lucastheborges@gmail.com>
2022-07-25 20:56:35 -0300

    Formulario de postagem

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,34 +1,50 @@
-import styles from './Post.module.css';
+import styles from "./Post.module.css";
 
-export function Post(){
-    return(
-        <article className={styles.post}>
-            <header>
-                <div className={styles.author}>
-                    <img className={styles.avatar} src='https://avatars.githubusercontent.com/u/61556295?v=4'/>
-                    <div className={styles.authorInfo}>
-                        <strong>Lucas Alberty</strong>
-                        <span>Web Developer</span>
-                    </div>
-                </div>
-                <time title='17 de Julho ás 08:13h' dateTime='17/07/2022 08:13:30'>Publicado há 1h</time>
-            </header>
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://avatars.githubusercontent.com/u/61556295?v=4"
+          />
+          <div className={styles.authorInfo}>
+            <strong>Lucas Alberty</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+        <time title="17 de Julho ás 08:13h" dateTime="17/07/2022 08:13:30">
+          Publicado há 1h
+        </time>
+      </header>
 
-            <div className={styles.content}>
-                
-                <p>Fala galeraa 👋</p>
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
 
-                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
 
-                <p>👉{" "} <a href='#'>jane.design/doctorcare</a></p>
+        <p>
+          👉 <a href="#">jane.design/doctorcare</a>
+        </p>
 
-                <p>
-                    <a href=''>#novoprojeto</a>{" "}
-                    <a href=''>#nlw</a>{" "}
-                    <a href=''>#rocketseat</a>
-                </p> 
+        <p>
+          <a href="">#novoprojeto</a> <a href="">#nlw</a>{" "}
+          <a href="">#rocketseat</a>
+        </p>
+      </div>
 
-            </div>
-        </article>
-    )
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea placeholder="Deixe um comentário" />
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
+    </article>
+  );
 }
